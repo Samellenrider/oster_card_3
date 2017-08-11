f25b2e05fd3928e381d18b19593a604e41cf29d5
Sam Ellenrieder <sam.ell@web.de>
2017-08-11 12:06:42 +0100

    Step 13, first steps

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,44 +1,98 @@
-  MAX_VALUE = 90
-  MIN_VALUE = 1
+MAX_VALUE = 90
+MIN_VALUE = 1
 
-  class Oystercard
-    attr_accessor :balance, :max_value, :in_use
+class Oystercard
+  attr_accessor :balance, :max_value, :journey_list, :entry_station, :exit_station
 
-   def initialize(balance = 0, in_use = false)
-      @balance = balance
-      @max_value = MAX_VALUE
-      @in_use = in_use
-    end
-
-   def top_up(value)
-      raise 'Top up limit exceeded!' if max?
-      @balance += value
-      "£#{balance}"
-    end
-
-   def max?
-      @balance >= max_value
-    end
-
-   def touch_in
-    if balance < MIN_VALUE
-      raise "Not enough money to travel."
-    else  
-      true
-    end
+  def initialize(balance = 0, _in_use = false, journey_list = [])
+    @balance = balance
+    @max_value = MAX_VALUE
+    @journey_list = journey_list
    end
 
-    def in_journey?
-      in_use == true
-    end
+  def top_up(value)
+    raise 'Top up limit exceeded!' if max?
+    @balance += value
+    "£#{balance}"
+   end
 
-    def touch_out 
-      deduct(MIN_VALUE)
-    end
+  def max?
+    @balance >= max_value
+   end
 
-    private
-    def deduct(amount)
-      @balance -= amount
-      balance
+  def touch_in(station)
+    if balance < MIN_VALUE
+      raise 'Not enough money to travel.'
+    else
+      @entry_station = station
+      true
     end
   end
+
+  def in_journey?
+    if !@entry_station.nil?
+      true
+    else
+      false
+    end
+  end
+
+  def touch_out(station)
+    deduct(MIN_VALUE)
+    @exit_station = station
+    journey_list.push(@entry_station => @exit_station)
+    @entry_station = nil
+  end
+
+  private
+
+  def deduct(amount)
+    @balance -= amount
+    balance
+  end
+end
+
+
+
+class Station
+
+  def initialize(station_name, zone)
+    @station_name = station_name
+    @zone = zone
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,7 +1,10 @@
 require 'oystercard'
 
-describe Oystercard do
+RSpec.describe Oystercard do
   subject { Oystercard.new }
+
+  let(:entry_station) { double :entry_station }
+  let(:exit_station) { double :exit_station }
 
   it 'checks if the Oyster has balance 0' do
     expect(subject.balance).to eq 0
@@ -23,27 +26,61 @@ describe Oystercard do
 
   it 'tells if we touched in' do
     subject.top_up(1)
-    expect(subject.touch_in).to eq true
+    expect(subject.touch_in(:entry_station)).to eq true
   end
 
   it "tells we're in journey" do
     t = Oystercard.new
-    expect(t.in_journey?).to eq(t.in_use)
+    t.top_up(1)
+    t.touch_in(:entry_station)
+    expect(t.in_journey?).to eq(true)
   end
 
   it 'tells if we touched out' do
     t = Oystercard.new
-    expect(t.touch_out).not_to eq(t.in_use)
-  end
-  
-  it "error when touch in balance = 0" do
-    t = Oystercard.new
-    expect{ t.touch_in }.to raise_error ("Not enough money to travel.")
+    t.top_up(1)
+    t.touch_in(:entry_station)
+    t.touch_out(:exit_station)
+    expect(t.exit_station).to eq(:exit_station)
   end
 
-  it "reduces balance by minimal amount" do
-    t =Oystercard.new
-    expect { t.touch_out }.to change{t.balance}.by(-1)
+  it 'error when touch in balance = 0' do
+    t = Oystercard.new
+    expect { t.touch_in(:entry_station) }.to raise_error 'Not enough money to travel.'
   end
-  
+
+  it 'reduces balance by minimal amount' do
+    t = Oystercard.new
+    t.top_up(1)
+    expect { t.touch_out(:exit_station) }.to change { t.balance }.by(-1)
+  end
+
+  it 'adds origin destination' do
+    t = Oystercard.new
+    t.top_up(1)
+    t.touch_in(:entry_station)
+    expect(t.entry_station).to eq :entry_station
+  end
+
+  it 'tells card to set entry station nil' do
+    t = Oystercard.new
+    t.top_up(1)
+    t.touch_in(:entry_station)
+    t.touch_out(:exit_station)
+    expect(t.entry_station).to eq nil
+  end
+
+  it 'have an empty list of journeys' do
+    t = Oystercard.new
+    t.top_up(1)
+    expect(t.journey_list).to eq []
+  end
+
+  it 'checks if touching in + touching out creates journey' do
+    t = Oystercard.new
+    t.top_up(1)
+    t.touch_in(:entry_station)
+    t.touch_out(:exit_station)
+    expect(t.journey_list).to eq t.journey_list.push(entry_station: :exit_station)
+  end
 end
