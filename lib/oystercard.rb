491b1aff4660f75fc50634fbc65f1bbfabf4a5d5
Sam Ellenrieder <sam.ell@web.de>
2017-08-11 13:59:52 +0100

    refactoring

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,13 +1,22 @@
+class Station
+  attr_reader :name, :zone
+
+  def initialize(station_name, zone)
+    @station_name = station_name
+    @zone = zone
+  end
+end
+
 MAX_VALUE = 90
 MIN_VALUE = 1
 
 class Oystercard
-  attr_accessor :balance, :max_value, :journey_list, :entry_station, :exit_station
+  attr_accessor :balance, :max_value, :journey_list
 
-  def initialize(balance = 0, _in_use = false, journey_list = [])
+  def initialize(balance = 0, _in_use = false)
     @balance = balance
     @max_value = MAX_VALUE
-    @journey_list = journey_list
+    @journey_list = []
    end
 
   def top_up(value)
@@ -24,75 +33,32 @@ class Oystercard
     if balance < MIN_VALUE
       raise 'Not enough money to travel.'
     else
-      @entry_station = station
-      true
+      @journey_list.push({in: station, out: nil})
     end
+  end
   end
 
   def in_journey?
-    if !@entry_station.nil?
-      true
-    else
+    if @journey_list = [] || if @journey_list.last[:out] != nil #we are acccessing the last element of the array
       false
+    else
+      true
     end
   end
 
   def touch_out(station)
     deduct(MIN_VALUE)
-    @exit_station = station
-    journey_list.push(@entry_station => @exit_station)
+    @journey_list.last[:out] = station
     @entry_station = nil
   end
 
-  private
+
+private
 
   def deduct(amount)
     @balance -= amount
     balance
   end
+
+
 end
-
-
-
-class Station
-
-  def initialize(station_name, zone)
-    @station_name = station_name
-    @zone = zone
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
